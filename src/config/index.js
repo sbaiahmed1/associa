8c90352f39545b32f89243d8ddff9d56c2326d7c
Ahmed <ahmedclubust@gmail.com>
2020-02-21 23:46:44 +0100

    INIT

---
+++ src/config/index.js
@@ -0,0 +1,6 @@
+import GlobalSheet from './globalStyles';
+import Colors from './colors';
+export {
+    GlobalSheet,
+    Colors
+}
---
+++ src/views/login/login.js
@@ -0,0 +1,101 @@
+import React, {Component} from 'react';
+import {
+  ScrollView,
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Keyboard,
+} from 'react-native';
+import loginStyle from './loginStyle';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import LinearGradient from 'react-native-linear-gradient';
+import {Colors, GlobalSheet} from '../../config';
+
+const facebook = (
+  <FontAwesome name={'facebook-f'} size={3 * GlobalSheet.units.vh} />
+);
+const google = <FontAwesome name={'google'} size={3 * GlobalSheet.units.vh} />;
+
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      position: 15 * GlobalSheet.units.vh,
+    };
+    this.setState = this.setState.bind(this);
+  }
+  componentDidMount(): void {
+    this.keyboardDidShowListener = Keyboard.addListener(
+      'keyboardDidShow',
+      this.keyboardDidShow,
+    );
+    this.keyboardDidHideListener = Keyboard.addListener(
+      'keyboardDidHide',
+      this.keyboardDismissed,
+    );
+  }
+
+  keyboardDidShow = () => {
+    this.setState({
+      position: -5 * GlobalSheet.units.vh,
+    });
+  };
+  keyboardDismissed = () => {
+    this.setState({
+      position: 70,
+    });
+  };
+  componentWillUnmount() {
+    this.keyboardDidShowListener.remove();
+    this.keyboardDidHideListener.remove();
+  }
+  render() {
+    return (
+      <View style={{flex: 1}}>
+        {/************************************************************************/}
+        <View style={{marginTop: this.state.position}}>
+          <Text style={loginStyle.titleStyle}>AssociaGest</Text>
+          <TextInput
+            style={loginStyle.textInputStyle}
+            placeholder="Example@email.com"
+            keyboardType={'email-address'}
+          />
+          <TextInput
+            onSubmitEditing={Keyboard.dismiss}
+            style={loginStyle.textInputStyle}
+            placeholder="********"
+            secureTextEntry={true}
+            returnKeyType
+          />
+          <View style={loginStyle.loginMethods}>
+            <TouchableOpacity style={loginStyle.facebook}>
+              <Text style={loginStyle.textFacebook}>{facebook}</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={loginStyle.signInButton}>
+              <LinearGradient
+                colors={[
+                  Colors.linearButton1,
+                  Colors.linearButton2,
+                  Colors.linearButton1,
+                ]}
+                style={loginStyle.gradientStyle}>
+                <Text style={loginStyle.textInsideGradient}>Sign In</Text>
+              </LinearGradient>
+            </TouchableOpacity>
+            <TouchableOpacity style={loginStyle.facebook}>
+              <Text style={loginStyle.textFacebook}>{google}</Text>
+            </TouchableOpacity>
+          </View>
+          <TouchableOpacity
+            style={{top: 30 * GlobalSheet.units.vh, alignSelf: 'center'}}>
+            <Text>Forgot Your Password?</Text>
+          </TouchableOpacity>
+        </View>
+        {/**************************************************************/}
+      </View>
+    );
+  }
+}
+
+export default Login;
